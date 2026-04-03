cb8ea8b319d2e7e14819fb1507f66f3ef51c2a4f
isabellagreco0 <igreco@udesa.edu.ar>
2026-04-03 10:06:36 -0300

    buscado pero no terminado

--- src/Components/Buscador/Buscador.js
+++ src/Components/Buscador/Buscador.js
@@ -1,24 +1,55 @@
 import React, { Component } from "react";
 
-class Detalle extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            algo: ""
-        };
-    }
+class Buscador extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      busqueda: "",
+      tipo: "movie"
+    };
+  }
 
-    algo() {
-        this.setState({
-            alto: ""
-        });
-    }
+  evitarSubmit(event) {
+    event.preventDefault();
 
-    render() {
-        return (
-            <div></div>
-        );
-    }
+    this.props.history.push( 
+      // NO SE QUE PONER ACA
+    );
+  }
+
+  controlarCambios(event) {
+    this.setState({
+      busqueda: event.target.value
+    });
+  }
+
+  cambiarTipo(event) {
+    this.setState({
+      tipo: event.target.value
+    });
+  }
+
+  render() {
+    return (
+      <form onSubmit={(event) => this.evitarSubmit(event)}>
+
+        <input
+          type="text"
+          placeholder="Buscar..."
+          value={this.state.busqueda}
+          onChange={(event) => this.controlarCambios(event)}
+        />
+
+        <select onChange={(event) => this.cambiarTipo(event)}>
+          <option value="movie">Películas</option>
+          <option value="tv">Series</option>
+        </select>
+
+        <button type="submit">Buscar</button>
+
+      </form>
+    );
+  }
 }
 
-export default Detalle;
+export default Buscador;
