3e12efb095cdfbf8437e5e2cd601044d4642389a
martubaumeister <martubaumeister@gmail.com>
2026-04-06 19:07:40 -0300

    modifiqué el header y algo más

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,31 +4,22 @@ import { Link } from 'react-router-dom';
 
 function Header (props) {
     return (
-        <li> <Link to={props.to}>{props.items}</Link> </li>
+        <nav>
+            <ul>
+                <li> <Link to="/">Homw</Link> </li>
+                <li> <Link to="">Peliculas</Link> </li>
+                <li> <Link to="">Series</Link> </li>
+                <li> <Link to="">Favoritas</Link> </li>
+            </ul>
+            <ul>
+                <li> <Link to="">Register</Link> </li>
+                <li> <Link to="">Login</Link> </li>
+            </ul>
+        </nav>
+        
+        
+        
     )
     }
 
 export default Header;
-
-<nav>
-            <ul class="nav nav-tabs my-4">
-                <li class="nav-item">
-                    <a class="nav-link" href="index.html">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="movies.html">Películas</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="series.html">Series</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="favorites.html">Favoritas</a>
-                </li>
-                <li class="nav-item ml-auto">
-                    <a class="nav-link" href="register.html">Registro</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="login.html">Login</a>
-                </li>
-            </ul>
-        </nav>
