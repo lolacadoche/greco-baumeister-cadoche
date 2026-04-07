a188a01142a79fd556a62ef5ed7b9bd55d039c80
isabellagreco0 <igreco@udesa.edu.ar>
2026-04-07 11:01:20 -0300

    movie

--- src/Components/Movie/Movie.js
+++ src/Components/Movie/Movie.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, {Component} from "react";
 import { Link } from "react-router-dom";
 
-function movie(props) {
-    return (
-        <section></section>
-    )
+class Movie extends Component {
+    constructor(props){
+        super(props)
+            }
+
+    render() {
+        return (
+            <article className='movie-card'>
+                <img src={props.image} alt={props.title} />
+
+                <h2>{props.title}</h2>
+
+                <p>{props.overview}</p>
+
+                <Link to={`/movie/${props.id}`}>
+                    <button className='more'>Ver más</button>
+                </Link>
+            </article>
+        )
+    }
 }
-export default movie
+
+export default Movie;
 
 
--- src/Screen/Peliculas/Peliculas.js
+++ src/Screen/Peliculas/Peliculas.js
@@ -1,17 +1,108 @@
 import React, { Component } from "react";
 import Buscador from "../../Components/Buscador/Buscador";
 import CargarMas from "../../Components/CargarMas/CargarMas";
+import Movie from "../../Components/Movies/Movies";
 
-function Peliculas() {
-  return (
-    <React.Fragment>
-      <h1>Todas las Pleiculas</h1>
-      <Buscador />
-      <CargarMas tipo="Movie"/>
+class Peliculas extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            peliculas: [],
+            copiapeliculas: [],
+            nextPage: 1,
+            filtro: ""
+        };
+    }
 
+    evitarSubmit(event) {
+        event.preventDefault();
+    }
 
-    </React.Fragment>
-  )
+    controlarCambios(event) {
+        this.setState({ filtro: event.target.value }, () => this.filtrarPeliculas());
+    }
+
+    filtrarPeliculas() {
+        this.setState({
+            peliculas: this.state.copiapeliculas.filter(pelicula =>
+                pelicula.title.toLowerCase().includes(this.state.filtro.toLowerCase())
+            )
+        })
+    }
+
+    componentDidMount() {
+        fetch('https://api.themoviedb.org/3/discover/movie?api_key=TU_API_KEY')
+            .then(response => response.json())
+            .then(data => this.setState({
+                peliculas: data.results,
+                copiapeliculas: data.results,
+                nextPage: 2
+            }))
+            .catch(error => console.log(error))
+    }
+
+    cargarMas = () => {
+        fetch(`https://api.themoviedb.org/3/discover/movie?api_key=TU_API_KEY&page=${this.state.nextPage}`)
+            .then(res => res.json())
+            .then(data => this.setState({
+                peliculas: this.state.peliculas.concat(data.results),
+                copiapeliculas: this.state.peliculas.concat(data.results),
+                nextPage: this.state.nextPage + 1
+            }))
+            .catch(err => console.log(err))
+    }
+
+    borrarPelicula = (id) => {
+        let filtradas = this.state.peliculas.filter(
+            pelicula => pelicula.id !== id)
+        this.setState({
+            peliculas: filtradas
+        })
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+
+                <form onSubmit={(event) => this.evitarSubmit(event)}>
+                    <label>Title:</label>
+                    <input
+                        type="text"
+                        onChange={(event) => this.controlarCambios(event)}
+                        value={this.state.filtro}
+                    />
+                </form>
+
+                <section className="personajeContainer">
+                    {this.state.peliculas.length > 0 ? (
+                        this.state.peliculas.map((pelicula) => (
+                            <Movie
+                                key={pelicula.id}
+                                id={pelicula.id}
+                                name={pelicula.title}
+                                image={`https://image.tmdb.org/t/p/w500${pelicula.poster_path}`}
+                                status={pelicula.release_date}
+                                species={pelicula.vote_average}
+                                origin={pelicula.original_language}
+                                borrar={this.borrarPelicula}
+                            />
+                        ))
+                    ) : (
+                        <p>Cargando...</p>
+                    )}
+                </section>
+
+                {
+                    this.state.nextPage && (
+                        <button onClick={this.cargarMas}>
+                            Mas Peliculas
+                        </button>
+                    )
+                }
+
+            </React.Fragment>
+        )
+    }
 }
 
 export default Peliculas;
