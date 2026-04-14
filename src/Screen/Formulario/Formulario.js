c24e8714ee0df1f54303ff8919969ea749957b86
isabellagreco0 <igreco@udesa.edu.ar>
2026-04-14 20:35:00 -0300

    trate de terminar search

--- src/Screen/Formulario/Formulario.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Formularios() {
-  return <h1>Formularios</h1>;
-}
-
-export default Formularios;
--- src/Screen/Search/Search.js
+++ src/Screen/Search/Search.js
@@ -7,12 +7,25 @@ class Search extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            resultados: []
+            resultados: [],
+            tipo: this.props.match.params.tipo,
+            contenido: ""
         };
     }
 
-componentDidMount(){
-    fetch(`https://api.themoviedb.org/3/movie/popular?api_key=cd21534ccf3ef8b078f7ac273cdf32ca&query `)
+    componentDidMount() {
+        let busqueda = this.props.match.params.busqueda;
+        let tipo = this.props.match.params.tipo;
+
+        let url = "";
+        if (tipo === "peliculas") {
+            url = `https://api.themoviedb.org/3/search/movie?api_key=cd21534ccf3ef8b078f7ac273cdf32ca&query=${busqueda}`
+        }
+        else if (tipo === "series") {
+            url = `https://api.themoviedb.org/3/search/tv?api_key=cd21534ccf3ef8b078f7ac273cdf32ca&query=${busqueda}`
+        }
+
+        fetch(url)
             .then((response) => response.json())
             .then((data) => {
                 this.setState({
@@ -23,36 +36,45 @@ componentDidMount(){
             .catch((error) => console.log("ERROR"));
     }
 
+
+
     render() {
-       
-          
-    let contenido = "";
+        let contenido = "";
+        if (this.state.resultados.length > 0) {
 
-    if (this.state.resultados.length > 0) {
-        contenido = (
-            <section className="card-Container">
-                {this.state.resultados.map((item, index) => (
-                    <Movie key={index} data={item} />
-                ))}
-            </section>
-    )
-    
-    } else {
-        contenido = (
-            <p>Lo siento. No hay resultados para tu busqueda.</p>
-        );
+            if (this.state.tipo === "series") {
+                contenido = (
+                    <section className="card-container">
+                        {this.state.resultados.map((item, index) => (<Serie key={index} data={item} />))}
+                    </section>
+                )
+            }
+
+            else if (this.state.tipo === "peliculas") {
+                contenido = (
+                    <section className="card-Container">
+                        {this.state.resultados.map((item, index) => (<Movie key={index} data={item} />))}
+                    </section>
+                )
+            }
+
+        } else {
+            contenido = (
+                <p>Lo siento. No hay resultados para tu busqueda.</p>
+            );
+        }
+
+
+        return (
+            <main>
+                <h1>Resultados de busqueda</h1>
+                {contenido}
+            </main>
+        )
+
+
+
     }
-
-
-    return (
-        <main>
-            <h1>Resultados de busqueda</h1>
-            {contenido}
-        </main>
-    )
-
-
-
-}}
+}
 
 export default Search
