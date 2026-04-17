6598416c36c6f94f0e6d44c39b97ba08b3b56666
martubaumeister <martubaumeister@gmail.com>
2026-04-17 13:56:15 -0300

    Copié la lógica para hacer aparecer/desaparecer los botones del header; favoritos, registrarse, login, logout

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,19 +3,35 @@ import { Link } from 'react-router-dom';
 
 
 function Header (props) {
+    let sesion= sessionStorage.getItem("usuarioEnSesion");
+    let userLogueado = false;
+
+    if(sesion !== null){
+        userLogueado=true; 
+    }
+    let linkFav = null; 
+    let linkLogin = null; 
+    let linkRegister = null; 
+    let linkLogout = null; 
+
+    if(userLogueado){
+        linkFav = <li className="nav-link"> <Link to="/Favoritos">Favoritas</Link> </li>
+        linkLogout = <li className="nav-item"> <Link to="/Home">Logout</Link> </li>
+    } else{
+        linkRegister = <li className="nav-link"> <Link to="/Register">Register</Link> </li>
+        linkLogin =<li className="nav-item"> <Link to="/Login">Login</Link> </li>
+    }
     return (
         <nav>
             <ul className="nav nav-tabs my-4">
                 <li className="nav-link"> <Link to="/">Home</Link> </li>
                 <li className="nav-link"> <Link to="/Peliculas">Peliculas</Link> </li>
                 <li className="nav-link"> <Link to="/Series">Series</Link> </li>
-
-                <li className="nav-link"> <Link to="/Favoritos">Favoritas</Link> </li>
-                <li className="nav-link"> <Link to="/Register">Register</Link> </li>
-                <li className="nav-item"> <Link to="/Login">Login</Link> </li>
+                {linkFav}
+                {linkRegister}
+                {linkLogin}
+                {linkLogout}
             </ul>
-
-        
         </nav>
         
         
